17fa674248fba410aeb078c66cdb805a70a7438b
neilha <neilharrold@gmail.com>
2024-08-16 10:28:44 +0100

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+    # The /api/content route is used for requests via the public API
+    get "/api/content(/*path_without_root)" => "content_items#show", :as => :content_item_api, :public_api_request => true
+
+    get "/content(/*path_without_root)" => "content_items#show", :as => :content_item  
+
+  get "up" => "rails/health#show", as: :rails_health_check
+end
